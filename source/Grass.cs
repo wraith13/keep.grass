ecbd41ceb93d20734891e082c252ab7b686e747f
道化師 <wraith@trickpalace.net>
2016-05-20 14:13:49 +0900

    最後の Public Activity のタイムスタンプを取得する処理 ( WIP )

---
+++ source/Grass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace keep.grass
+{
+	public class Grass
+	{
+		static private string AtomUrlFormat = "https://github.com/{0}.atom";
+		static public string AtomUrl(string Id)
+		{
+			return String.Format (AtomUrlFormat, Id);
+		}
+		static public DateTime GetLastPublicActivity(string Id)
+		{
+		}
+	}
+}
+
