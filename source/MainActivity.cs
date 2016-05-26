8a6d887fc26a799b015f77978fa8f4eb0f3b5c40
道化師 <wraith@trickpalace.net>
2016-05-26 13:55:47 +0900

    素の android app として作り直し
    どうにも android wear ベースのひな形を使うといろいろ全然うまくいかないので。

--- source/MainActivity.cs
+++ source/MainActivity.cs
@@ -1,50 +1,31 @@
-﻿using System;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Support.V4.App;
-using Android.Support.Wearable.Views;
-using Android.Views;
+﻿using Android.App;
 using Android.Widget;
+using Android.OS;
 
 namespace keep.grass
 {
-	[Activity (Label = "KeepGrass", MainLauncher = true, Icon = "@mipmap/icon")]
+	[Activity(Label = "primeapp", MainLauncher = true, Icon = "@mipmap/icon")]
 	public class MainActivity : Activity
 	{
 		int count = 1;
 
-		protected override void OnCreate (Bundle bundle)
+		protected override void OnCreate(Bundle savedInstanceState)
 		{
-			base.OnCreate (bundle);
+			base.OnCreate(savedInstanceState);
 
 			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
+			SetContentView(Resource.Layout.Main);
 
-			var v = FindViewById<WatchViewStub> (Resource.Id.watch_view_stub);
-			v.LayoutInflated += delegate {
-
-				// Get our button from the layout resource,
-				// and attach an event to it
-				Button button = FindViewById<Button> (Resource.Id.myButton);
-				
-				button.Click += delegate {
-					var notification = new NotificationCompat.Builder (this)
-						.SetContentTitle ("Button tapped")
-						.SetContentText ("Button tapped " + count++ + " times!")
-						.SetSmallIcon (Android.Resource.Drawable.StatNotifyVoicemail)
-						.SetGroup ("group_key_demo").Build ();
-
-					var manager = NotificationManagerCompat.From (this);
-					manager.Notify (1, notification);
-					button.Text = "Check Notification!";
-				};
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button>(Resource.Id.myButton);
+			
+			button.Click += delegate
+			{
+				button.Text = string.Format("{0} clicks!", count++);
 			};
 		}
 	}
 }
-
 
 
