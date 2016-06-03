c0b80b136cdee8fe78125e824a33fe8f96ae959f
道化師 <wraith@trickpalace.net>
2016-06-03 09:56:21 +0900

    android版の名前から .Droid を除去

--- source/Droid/MainActivity.cs
+++ source/Droid/MainActivity.cs
@@ -10,7 +10,7 @@ using Android.OS;
 
 namespace keep.grass.Droid
 {
-	[Activity(Label = "keep.grass.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity(Label = "keep.grass", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
 	{
 		protected override void OnCreate(Bundle bundle)
