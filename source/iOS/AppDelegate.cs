e271ef7dc1c880dc91acf8bf9ff5308b742172dc
道化師 <wraith@trickpalace.net>
2016-06-09 13:36:10 +0900

    iOS での通知パーミッションの取得

--- source/iOS/AppDelegate.cs
+++ source/iOS/AppDelegate.cs
@@ -16,6 +16,16 @@ namespace keep.grass.iOS
 
 			LoadApplication(new App());
 
+			// copy from http://www.knowing.net/index.php/2014/07/03/local-notifications-in-ios-8-with-xamarin/
+			var settings = UIUserNotificationSettings.GetSettingsForTypes
+				(
+					UIUserNotificationType.Alert
+					| UIUserNotificationType.Badge
+					| UIUserNotificationType.Sound,
+					new NSSet()
+				);
+			UIApplication.SharedApplication.RegisterUserNotificationSettings(settings);
+
 			return base.FinishedLaunching(app, options);
 		}
 	}
