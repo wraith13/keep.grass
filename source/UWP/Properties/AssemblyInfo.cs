b908e1084f6097192c557c30dad642ad8f55d672
道化師 <wraith@trickpalace.net>
2016-06-12 06:32:06 +0900

    UWP プロジェクトの各種設定

--- source/UWP/Properties/AssemblyInfo.cs
+++ source/UWP/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("keep.grass.UWP")]
+[assembly: AssemblyTitle("keep.grass")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("keep.grass.UWP")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("keep.grass")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
