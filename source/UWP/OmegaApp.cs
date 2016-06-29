3c3bf6ce13cc1f8695a617f3f7214b13b5d2b431
道化師 <wraith@trickpalace.net>
2016-06-29 23:46:02 +0900

    UWP版で通知が表示されない問題の修正

--- source/UWP/OmegaApp.cs
+++ source/UWP/OmegaApp.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Windows.UI.Notifications;
+
 namespace keep.grass.UWP
 {
     public class OmegaApp : AlphaApp
@@ -15,6 +17,27 @@ namespace keep.grass.UWP
         public override string getLanguage()
         {
             return Windows.System.UserProfile.GlobalizationPreferences.Languages[0].Split('-')[0];
+        }
+
+        public override void ShowAlerts(string title, string body, int id, DateTime notifyTime)
+        {
+            //  こんなコードを自前で用意しない為に Plugin.LocalNotifications を導入したのだが。。。
+            //  UWP にも対応してるハズなのになぜか動作してくれない。
+
+            //  base code https://msdn.microsoft.com/ja-jp/library/windows/desktop/windows.ui.notifications.scheduledtoastnotification?cs-save-lang=1&cs-lang=csharp#code-snippet-1
+
+            // Set up the notification text.
+            var toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText02);
+            var strings = toastXml.GetElementsByTagName("text");
+            strings[0].AppendChild(toastXml.CreateTextNode(title));
+            strings[1].AppendChild(toastXml.CreateTextNode(body));
+
+            // Create the toast notification object.
+            var toast = new ScheduledToastNotification(toastXml, notifyTime);
+            toast.Id = "Toast" + id.ToString();
+
+            // Add to the schedule.
+            ToastNotificationManager.CreateToastNotifier().AddToSchedule(toast);
         }
     }
 }
