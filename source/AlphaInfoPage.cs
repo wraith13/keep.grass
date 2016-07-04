ca9b398b2bbd4ae7d4dfd0c3b68e33729d4853f5
道化師 <wraith@trickpalace.net>
2016-07-04 13:49:32 +0900

    infoページ

--- source/AlphaInfoPage.cs
+++ source/AlphaInfoPage.cs
@@ -13,10 +13,20 @@ namespace keep.grass
 			Root = AppRoot;
 			L = Root.L;
 			Title = L["information"];
+
+			var version = AlphaFactory.MakeCircleImageCell();
+			version.ImageSource = "https://raw.githubusercontent.com/wraith13/keep.grass/master/source/iOS/Resources/Images.xcassets/AppIcons.appiconset/keep.grass.180.png";
+			version.Text = "1.00.000";
+
 			var twitter = AlphaFactory.MakeCircleImageCell();
 			twitter.ImageSource = GitHub.GetIconUrl("wraith13");
 			twitter.Text = "@wraith13";
 			twitter.Command = new Command(o => Device.OpenUri(new Uri("https://twitter.com/wraith13")));
+
+			var github = AlphaFactory.MakeCircleImageCell();
+			github.ImageSource = "https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png";
+			github.Text = "wraith13/keep.grass";
+			github.Command = new Command(o => Device.OpenUri(new Uri("https://github.com/wraith13/keep.grass")));
 
 			Content = new StackLayout
 			{
@@ -28,10 +38,7 @@ namespace keep.grass
 						{
 							new TableSection(L["Version"])
 							{
-								new TextCell
-								{
-									Text = "1.00.000",
-								}
+								version
 							},
 							new TableSection(L["Auther"])
 							{
@@ -39,10 +46,7 @@ namespace keep.grass
 							},
 							new TableSection(L["Github Repository"])
 							{
-								new TextCell
-								{
-									Text = "https://github.com/wraith13/keep.grass",
-								}
+								github
 							},
 						}
 					},
