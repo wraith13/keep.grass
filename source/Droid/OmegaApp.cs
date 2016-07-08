1503f231236e35ebe11acb15d953c5e7b6fd05db
道化師 <wraith@trickpalace.net>
2016-07-08 13:58:32 +0900

    Android版の通知の Native 化 ( WIP )

--- source/Droid/OmegaApp.cs
+++ source/Droid/OmegaApp.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Java.Util;
+using Android.App;
+using Android.Content;
 
 namespace keep.grass.Droid
 {
@@ -12,6 +14,29 @@ namespace keep.grass.Droid
 		public override string getLanguage()
 		{
 			return Locale.Default.ToString().Split('_')[0];
+		}
+
+		public override void ShowAlert(string title, string body, int id, DateTime notifyTime)
+		{
+			var builder = new Notification.Builder(Xamarin.Forms.Forms.Context);
+			builder.SetContentTitle(title);
+			builder.SetContentText(body);
+			builder.SetGroup("keep.grass.alert");
+			//builder.SetLargeIcon();
+
+			var notification = builder.Build();
+			var notificationIntent = new Intent(Xamarin.Forms.Forms.Context, typeof(MainActivity));
+			notificationIntent.PutExtra(NotificationPublisher.NOTIFICATION_ID, 1);
+		    notificationIntent.PutExtra(NotificationPublisher.NOTIFICATION, notification);
+		    PendingIntent pendingIntent = PendingIntent.GetBroadcast(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+			long futureInMillis = SystemClock.elapsedRealtime() + delay;
+			AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+			alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, pendingIntent);
+		
+		}
+		public override void CancelAlert(int id)
+		{
 		}
 	}
 }
