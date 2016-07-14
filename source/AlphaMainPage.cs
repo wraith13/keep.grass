f365c7e4119f4715b3dd5adea6f7192945356db1
道化師 <wraith@trickpalace.net>
2016-07-14 21:18:11 +0900

    更新ボタンと設定ボタンを横並びに

--- source/AlphaMainPage.cs
+++ source/AlphaMainPage.cs
@@ -29,6 +29,49 @@ namespace keep.grass
 			UserLabel.Command = new Command(o => Root.ShowSettingsPage());
 			LastActivityStampLabel.Command = new Command(o => UpdateLastPublicActivityAsync().Wait(0));
 
+			var ButtonGrid = new Grid
+			{
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				VerticalOptions = LayoutOptions.Center,
+				RowDefinitions =
+				{
+					new RowDefinition() { Height = GridLength.Auto }
+				},
+				ColumnDefinitions =
+				{
+					new ColumnDefinition
+					{
+						Width = new GridLength(1, GridUnitType.Star),
+					},
+					new ColumnDefinition
+					{
+						Width = new GridLength(1, GridUnitType.Star),
+					},
+				},
+			};
+			ButtonGrid.Children.Add
+			(
+				new Button
+				{
+					VerticalOptions = LayoutOptions.Center,
+					HorizontalOptions = LayoutOptions.FillAndExpand,
+					Text = L["Update"],
+					Command = LastActivityStampLabel.Command,
+				},
+				0, 0
+			);
+			ButtonGrid.Children.Add
+			(
+				new Button
+				{
+					VerticalOptions = LayoutOptions.Center,
+					HorizontalOptions = LayoutOptions.FillAndExpand,
+					Text = L["Settings"],
+					Command = UserLabel.Command,
+				},
+				1, 0
+			);
+
 			Content = new StackLayout { 
 				Children =
 				{
@@ -50,16 +93,7 @@ namespace keep.grass
 							},
 						},
 					},
-					new Button
-					{
-						Text = L["Update"],
-						Command = LastActivityStampLabel.Command,
-					},
-					new Button
-					{
-						Text = L["Settings"],
-						Command = UserLabel.Command,
-					},
+					ButtonGrid
 				},
 			};
 		}
@@ -186,5 +220,3 @@ namespace keep.grass
 		}
 	}
 }
-
-
