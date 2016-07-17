dfc116064c428519a83cf5c292a5225599b85a1c
道化師 <wraith@trickpalace.net>
2016-07-17 14:48:05 +0900

    iOS版の通知の Native 化

--- source/iOS/OmegaApp.cs
+++ source/iOS/OmegaApp.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Foundation;
+using UIKit;
 
 namespace keep.grass.iOS
 {
@@ -16,13 +19,35 @@ namespace keep.grass.iOS
 
 		public override void ShowAlert(string title, string body, int id, DateTime notifyTime)
 		{
-			base.ShowAlert
-		    (
-				"keep.grass",
-				title + ",\r\n" + body,
-				id,
-				notifyTime
-		   );
+			CancelAlert(id);
+			UIApplication.SharedApplication.ScheduleLocalNotification
+			(
+				new UILocalNotification
+				{
+					//AlertTitle = title, // iOS 9 からは通知センターでしかこの AlertTitle は有効にならず、それ以外の箇所ではアプリ名が代わりに表示され使い物にならない。
+					AlertBody = title +"\r\n" +body,
+					FireDate = NSDate.Now.AddSeconds((notifyTime - DateTime.Now).TotalSeconds),
+					TimeZone = NSTimeZone.DefaultTimeZone,
+					UserInfo = new NSDictionary("id", id.ToString()),
+				}
+			);
+		}
+		public override void CancelAlert(int id)
+		{
+			foreach
+			(
+				var notification
+				in UIApplication.SharedApplication.ScheduledLocalNotifications
+					.Where
+					(
+						n =>
+							n.UserInfo.ContainsKey(new NSString("id")) &&
+							n.UserInfo.ValueForKey(new NSString("id")).ToString() == id.ToString()
+					)
+			)
+			{
+				UIApplication.SharedApplication.CancelLocalNotification(notification);
+			}
 		}
 	}
 }
