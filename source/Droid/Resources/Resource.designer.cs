093783f9b791ec09da5b1f4a02243739fc431f29
道化師 <wraith@trickpalace.net>
2016-07-19 20:45:54 +0900

    Androidプロジェクトから Local Notifications Plugin パッケージの除去

--- source/Droid/Resources/Resource.designer.cs
+++ source/Droid/Resources/Resource.designer.cs
@@ -26,7 +26,6 @@ namespace keep.grass.Droid
 		
 		public static void UpdateIdValues()
 		{
-			global::Plugin.LocalNotifications.Resource.Drawable.plugin_lc_smallicon = global::keep.grass.Droid.Resource.Drawable.plugin_lc_smallicon;
 			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::keep.grass.Droid.Resource.Attribute.actionBarSize;
 		}
 		
@@ -2201,11 +2200,8 @@ namespace keep.grass.Droid
 			// aapt resource value: 0x7f0200a0
 			public const int mr_ic_play_light = 2130837664;
 			
-			// aapt resource value: 0x7f0200a2
-			public const int notification_template_icon_bg = 2130837666;
-			
 			// aapt resource value: 0x7f0200a1
-			public const int plugin_lc_smallicon = 2130837665;
+			public const int notification_template_icon_bg = 2130837665;
 			
 			static Drawable()
 			{
