f87bafa016657e05a025829f5ebf9c897f881f3a
道化師 <wraith@trickpalace.net>
2016-08-04 22:43:25 +0900

    ResponsiveLayout ( WIP )

---
+++ source/ResponsiveLayout.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace keep.grass
+{
+	public class ResponsiveLayout : StackLayout
+	{
+		public int MaxColumnWidth;
+		public int MinColumnWidth;
+
+		public List<Layout> BlockList = new List<Layout>();
+		public List<StackLayout> ColumnStackList = new List<StackLayout>();
+
+		public ResponsiveLayout()
+		{
+			Orientation = StackOrientation.Horizontal;
+		}
+	}
+}
+
