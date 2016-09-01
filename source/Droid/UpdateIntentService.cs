12cff178c8e3043fd7e3d156c6fd3bb8d06994e8
道化師 <wraith@trickpalace.net>
2016-09-01 14:06:41 +0900

    Androidでのバックグラウンド更新( WIP )

---
+++ source/Droid/UpdateIntentService.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Xamarin.Forms;
+
+namespace keep.grass.Droid
+{
+	[Service]
+	public class AlarmIntentService :IntentService
+	{
+		protected override void OnHandleIntent(Intent intent)
+		{
+			Debug.WriteLine("AlarmIntentService::OnHandleIntent()");
+			try
+			{
+				ShowNotification(ApplicationContext, intent);
+			}
+			finally
+			{
+				Android.Support.V4.Content.WakefulBroadcastReceiver.CompleteWakefulIntent(intent);
+			}
+		}
+		public void ShowNotification(Context context, Intent intent)
+		{
+			var id = intent.GetIntExtra("id", 0);
+			var title = intent.GetStringExtra("title");
+			var body = intent.GetStringExtra("message");
+
+			var main_intent = new Intent(context, typeof(MainActivity));
+			main_intent.PutExtra("type", "alarm");
+			main_intent.PutExtra("id", id);
+
+			var builder = new Notification.Builder(Forms.Context);
+			//builder.SetGroup("keep.grass");
+			builder.SetTicker(title);
+			builder.SetContentTitle(title);
+			builder.SetContentText(body);
+			builder.SetContentIntent
+			(
+				PendingIntent.GetActivity
+				(
+					context,
+					id,
+					main_intent,
+					PendingIntentFlags.UpdateCurrent
+				)
+			);
+			builder.SetSmallIcon(Resource.Drawable.icon);
+			builder.SetLargeIcon(BitmapFactory.DecodeResource(context.Resources, Resource.Drawable.icon));
+			builder.SetDefaults(NotificationDefaults.All);
+			builder.SetAutoCancel(true);
+
+			((NotificationManager)context.GetSystemService(NotificationService))
+				.Notify(id, builder.Build());
+		}
+	}
+}
+
---
+++ source/Droid/UpdateWakefulReceiver.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using Android.Content;
+using Android.Support.V4.Content;
+
+namespace keep.grass.Droid
+{
+	[BroadcastReceiver]
+	public class AlarmWakefulReceiver : WakefulBroadcastReceiver
+	{
+		public override void OnReceive(Context context, Intent intent)
+		{
+			Debug.WriteLine("AlarmWakefulReceiver::OnReceive()");
+			var intent2 = new Intent(context, typeof(AlarmIntentService));
+			intent2.PutExtra("id", intent.GetIntExtra("id", 0));
+			intent2.PutExtra("title", intent.GetStringExtra("title"));
+			intent2.PutExtra("message", intent.GetStringExtra("message"));
+			StartWakefulService(context, intent2);
+		}
+	}
+}
+
