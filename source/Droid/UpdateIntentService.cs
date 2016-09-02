0f06e010753b173d46a38f186d26133c81d20104
道化師 <wraith@trickpalace.net>
2016-09-02 09:59:40 +0900

    Androidでのバックグラウンド更新( WIP )

--- source/Droid/UpdateIntentService.cs
+++ source/Droid/UpdateIntentService.cs
@@ -14,45 +14,15 @@ namespace keep.grass.Droid
 			Debug.WriteLine("UpdateIntentService::OnHandleIntent()");
 			try
 			{
-				ShowNotification(ApplicationContext, intent);
+				UpdateLastPublicActivity(ApplicationContext, intent);
 			}
 			finally
 			{
 				Android.Support.V4.Content.WakefulBroadcastReceiver.CompleteWakefulIntent(intent);
 			}
 		}
-		public void ShowNotification(Context context, Intent intent)
+		public void UpdateLastPublicActivity(Context context, Intent intent)
 		{
-			var id = intent.GetIntExtra("id", 0);
-			var title = intent.GetStringExtra("title");
-			var body = intent.GetStringExtra("message");
-
-			var main_intent = new Intent(context, typeof(MainActivity));
-			main_intent.PutExtra("type", "alarm");
-			main_intent.PutExtra("id", id);
-
-			var builder = new Notification.Builder(Forms.Context);
-			//builder.SetGroup("keep.grass");
-			builder.SetTicker(title);
-			builder.SetContentTitle(title);
-			builder.SetContentText(body);
-			builder.SetContentIntent
-			(
-				PendingIntent.GetActivity
-				(
-					context,
-					id,
-					main_intent,
-					PendingIntentFlags.UpdateCurrent
-				)
-			);
-			builder.SetSmallIcon(Resource.Drawable.icon);
-			builder.SetLargeIcon(BitmapFactory.DecodeResource(context.Resources, Resource.Drawable.icon));
-			builder.SetDefaults(NotificationDefaults.All);
-			builder.SetAutoCancel(true);
-
-			((NotificationManager)context.GetSystemService(NotificationService))
-				.Notify(id, builder.Build());
 		}
 	}
 }
