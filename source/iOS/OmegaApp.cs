cceb246fe21c42782e91a1adc61e32cb2eafc3fa
道化師 <wraith@trickpalace.net>
2016-09-07 12:43:55 +0900

    GetApplicationStoreUri() を App から Domain へ引っ越し ( iOS )

--- source/iOS/OmegaApp.cs
+++ source/iOS/OmegaApp.cs
@@ -11,12 +11,6 @@ namespace keep.grass.iOS
 		public OmegaApp()
 		{
 		}
-
-		public override Uri GetApplicationStoreUri()
-		{
-			//	暫定実装。これは実際には twitter 公式アプリの URL
-			return new Uri("https://itunes.apple.com/jp/app/twitter/id333903271?mt=8");
-		}
 	}
 }
 
--- source/iOS/OmegaDomain.cs
+++ source/iOS/OmegaDomain.cs
@@ -43,6 +43,12 @@ namespace keep.grass.iOS
 		{
 			UIApplication.SharedApplication.CancelAllLocalNotifications();
 		}
+
+		public override Uri GetApplicationStoreUri()
+		{
+			//	暫定実装。これは実際には twitter 公式アプリの URL
+			return new Uri("https://itunes.apple.com/jp/app/twitter/id333903271?mt=8");
+		}
 	}
 }
 
