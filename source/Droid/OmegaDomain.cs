54935c168f30e419f28cd9f2c212c6af60a9a441
道化師 <wraith@trickpalace.net>
2016-09-08 13:14:45 +0900

    Android 版での自動更新タイミングの調整
    各アラートの2分前 → (各アラート + １時間毎)の2分前

--- source/Droid/OmegaDomain.cs
+++ source/Droid/OmegaDomain.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using Android.App;
 using Android.Content;
 using Android.OS;
+using keep.grass.Helpers;
 using Xamarin.Forms;
 
 namespace keep.grass.Droid
@@ -19,6 +21,56 @@ namespace keep.grass.Droid
 		}
 
 		const int BaseUpdateId = 1000;
+
+		public static long ToAndroidTicks(DateTime at)
+		{
+			return SystemClock.ElapsedRealtime() +
+				((long)(at -DateTime.Now).TotalMilliseconds);
+		}
+
+		public override void UpdateAlerts()
+		{
+			base.UpdateAlerts();
+			if
+			(
+				String.IsNullOrWhiteSpace(Settings.UserName) ||
+				default(DateTime) == LastPublicActivity
+			)
+			{
+				System.Diagnostics.Debug.WriteLine("OmegaDomain::CancelAllAlerts");
+			}
+			else
+			{
+				System.Diagnostics.Debug.WriteLine("OmegaDomain::UpdateAlerts");
+				var Limit = LastPublicActivity.AddHours(24);
+				var Now = DateTime.Now;
+				int id = BaseUpdateId;
+				foreach
+				(
+					var Span in
+						Settings.AlertTimeSpanTable
+							.Union(Enumerable.Range(0, 24).Select(i => TimeSpan.FromHours(i)))
+							.Where(i => Now < Limit.Add(-i).AddSeconds(-300)) // 基準時刻が過去あるいは5分以内場合は実行しない
+				)
+				{
+					//	基準時刻の90秒前に
+					var updateAt = Limit -(Span + TimeSpan.FromSeconds(90));
+
+					GetAlarmManager().Set
+					(
+						AlarmType.ElapsedRealtime,
+						ToAndroidTicks(updateAt),
+						PendingIntent.GetBroadcast
+						(
+							Forms.Context,
+							id++,
+							new Intent(Context, typeof(UpdateWakefulReceiver)),
+							PendingIntentFlags.UpdateCurrent
+						)
+				   );
+				}
+			}
+		}
 
 		public AlarmManager GetAlarmManager()
 		{
@@ -47,29 +99,12 @@ namespace keep.grass.Droid
 		}
 		public override void ShowAlert(string title, string body, int id, DateTime notifyTime)
 		{
-			var alertAt = SystemClock.ElapsedRealtime() + (long)((notifyTime - DateTime.Now).TotalMilliseconds);
 			GetAlarmManager().Set
 			(
 				AlarmType.ElapsedRealtime,
-				alertAt,
+				ToAndroidTicks(notifyTime),
 				MakeAlarmIntent(id, title, body)
 		   	);
-			if (TimeSpan.FromSeconds(300) <= notifyTime -DateTime.Now)
-			{
-				var updateAt = alertAt - (long)TimeSpan.FromSeconds(120).TotalMilliseconds;
-				GetAlarmManager().Set
-				(
-					AlarmType.ElapsedRealtime,
-					updateAt,
-					PendingIntent.GetBroadcast
-					(
-						Forms.Context,
-						id + BaseUpdateId,
-						new Intent(Context, typeof(UpdateWakefulReceiver)),
-						PendingIntentFlags.UpdateCurrent
-					)
-			   );
-			}
 		}
 		public override void CancelAlert(int id)
 		{
