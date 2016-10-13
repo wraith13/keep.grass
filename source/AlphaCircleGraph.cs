5c77c8b85de7cd3807d87a8c08b78328f9a3b8ea
道化師 <wraith@trickpalace.net>
2016-10-13 09:24:25 +0900

    AlphaCircleGraph ( WIP )

---
+++ source/AlphaCircleGraph.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace keep.grass
+{
+	public class AlphaPie
+	{
+		string Text { get; set; }
+		double Volume { get; set; }
+		string DisplayVolume { get; set; }
+	}
+	public interface VoidCircleGraph
+	{
+		IEnumerable<AlphaPie> Data { get; set; }
+		View AsView();
+	}
+	public class AlphaCircleGraph
+	{
+		public AlphaCircleGraph()
+		{
+		}
+	}
+}
