6dc94aae928dbe8b3d660bb70772b1d8be27d961
道化師 <wraith@trickpalace.net>
2016-10-16 01:50:00 +0900

    AlphaCircleGraph ( WIP )

--- source/AlphaCircleGraph.cs
+++ source/AlphaCircleGraph.cs
@@ -7,6 +7,8 @@ using Xamarin.Forms;
 using OxyPlot;
 using OxyPlot.Series;
 using OxyPlot.Xamarin.Forms;
+#else
+using SkiaSharp;
 #endif
 
 
@@ -52,7 +54,8 @@ namespace keep.grass
 			);
 		}
 	}
-	public class AlphaCircleGraph :View, VoidCircleGraph
+#if USE_OXYPLOT
+	public class AlphaCircleGraph :VoidCircleGraph
 	{
 		IEnumerable<VoidPie> Pies;
 
@@ -139,4 +142,86 @@ namespace keep.grass
 			return GrahpFrame;
 		}
 	}
+#else
+	public class AlphaCircleGraph :VoidCircleGraph
+	{
+		IEnumerable<VoidPie> Pies;
+		Image Image;
+		double GraphSize;
+
+		public AlphaCircleGraph()
+		{
+		}
+		public void Build(double Width, double Height)
+		{
+			GraphSize = new[] { Width, Height }.Min() * 0.6;
+			Image = new Image()
+			{
+				WidthRequest = GraphSize,
+				HeightRequest = GraphSize,
+				BackgroundColor = Color.White,
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				VerticalOptions = LayoutOptions.FillAndExpand,
+			};
+			Update();
+		}
+
+		public IEnumerable<VoidPie> Data
+		{
+			get
+			{
+				return Pies;
+			}
+
+			set
+			{
+				Pies = value;
+				Update();
+			}
+		}
+
+		public void Update()
+		{
+			using (var surface = SKSurface.Create((int)GraphSize, (int)GraphSize, SKColorType.Rgba8888, SKAlphaType.Premul))
+			{
+				var canvas = surface.Canvas;
+				using (var paint = new SKPaint())
+				{
+					paint.IsAntialias = true;
+					paint.Color = new SKColor(0x2c, 0x3e, 0x50);
+					paint.StrokeCap = SKStrokeCap.Round;
+
+					// create the Xamagon path
+					using (var path = new SKPath())
+					{
+						path.MoveTo(71.4311121f, 56f);
+						path.CubicTo(68.6763107f, 56.0058575f, 65.9796704f, 57.5737917f, 64.5928855f, 59.965729f);
+						path.LineTo(43.0238921f, 97.5342563f);
+						path.CubicTo(41.6587026f, 99.9325978f, 41.6587026f, 103.067402f, 43.0238921f, 105.465744f);
+						path.LineTo(64.5928855f, 143.034271f);
+						path.CubicTo(65.9798162f, 145.426228f, 68.6763107f, 146.994582f, 71.4311121f, 147f);
+						path.LineTo(114.568946f, 147f);
+						path.CubicTo(117.323748f, 146.994143f, 120.020241f, 145.426228f, 121.407172f, 143.034271f);
+						path.LineTo(142.976161f, 105.465744f);
+						path.CubicTo(144.34135f, 103.067402f, 144.341209f, 99.9325978f, 142.976161f, 97.5342563f);
+						path.LineTo(121.407172f, 59.965729f);
+						path.CubicTo(120.020241f, 57.5737917f, 117.323748f, 56.0054182f, 114.568946f, 56f);
+						path.LineTo(71.4311121f, 56f);
+						path.Close();
+
+						// draw the Xamagon path
+						canvas.DrawPath(path, paint);
+					}
+				}
+				var ImageData = surface.Snapshot().Encode();
+				Image.Source = ImageSource.FromStream(() => ImageData.AsStream());
+			}
+		}
+
+		public View AsView()
+		{
+			return Image;
+		}
+	}
+#endif
 }
