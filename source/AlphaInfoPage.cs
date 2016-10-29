086e9df472cee4633001d79a09b1832d0c48c269
道化師 <wraith@trickpalace.net>
2016-10-29 23:11:03 +0900

    情報画面のレスポンシブレイアウト対応

--- source/AlphaInfoPage.cs
+++ source/AlphaInfoPage.cs
@@ -1,16 +1,60 @@
 ﻿using System;
+using System.Diagnostics;
 using Xamarin.Forms;
 
 namespace keep.grass
 {
-	public class AlphaInfoPage : ContentPage
+	public class AlphaInfoPage : ResponsiveContentPage
 	{
+		AlphaApp Root = AlphaFactory.MakeSureApp();
+		Languages.AlphaLanguage L = AlphaFactory.MakeSureLanguage();
+
 		public AlphaInfoPage()
 		{
-			AlphaApp Root = AlphaFactory.MakeSureApp();
-			Languages.AlphaLanguage L = AlphaFactory.MakeSureLanguage();
-
 			Title = L["Information"];
+		}
+		public override void Build()
+		{
+			base.Build();
+			Debug.WriteLine("AlphaInfoPage.Rebuild();");
+
+			var Version = new TableSection(L["Version"])
+			{
+				AlphaFactory.MakeCircleImageCell
+				(
+					ImageSource: Root.GetApplicationImageSource(),
+					Text: "1.00.000",
+					Command: new Command
+					(
+						o => Device.OpenUri
+						(
+							AlphaFactory.MakeSureDomain().GetApplicationStoreUri()
+						)
+					),
+					OptionImageSource: Root.GetExportImageSource()
+				),
+			};
+			var Auther = new TableSection(L["Auther"])
+			{
+				AlphaFactory.MakeCircleImageCell
+				(
+					ImageSource: Root.GetWraithImageSource(),
+					Text: "@wraith13",
+					Command: new Command(o => Device.OpenUri(new Uri("https://twitter.com/wraith13"))),
+					OptionImageSource: Root.GetExportImageSource()
+				),
+			};
+			var Repository = new TableSection(L["Github Repository"])
+			{
+				AlphaFactory.MakeCircleImageCell
+				(
+					ImageSource: Root.GetGitHubImageSource(),
+					Text: "wraith13/keep.grass",
+					Command: new Command(o => Device.OpenUri(new Uri("https://github.com/wraith13/keep.grass"))),
+					OptionImageSource: Root.GetExportImageSource()
+				),
+			};
+
 			Content = new StackLayout
 			{
 				Children =
@@ -19,42 +63,9 @@ namespace keep.grass
 					{
 						Root = new TableRoot
 						{
-							new TableSection(L["Version"])
-							{
-								AlphaFactory.MakeCircleImageCell
-								(
-									ImageSource: Root.GetApplicationImageSource(),
-									Text: "1.00.000",
-									Command: new Command
-									(
-										o => Device.OpenUri
-										(
-											AlphaFactory.MakeSureDomain().GetApplicationStoreUri()
-										)
-									),
-						            OptionImageSource: Root.GetExportImageSource()
-								),
-							},
-							new TableSection(L["Auther"])
-							{
-								AlphaFactory.MakeCircleImageCell
-								(
-									ImageSource: Root.GetWraithImageSource(),
-									Text: "@wraith13",
-									Command: new Command(o => Device.OpenUri(new Uri("https://twitter.com/wraith13"))),
-									OptionImageSource: Root.GetExportImageSource()
-								),
-							},
-							new TableSection(L["Github Repository"])
-							{
-								AlphaFactory.MakeCircleImageCell
-								(
-									ImageSource: Root.GetGitHubImageSource(),
-									Text: "wraith13/keep.grass",
-									Command: new Command(o => Device.OpenUri(new Uri("https://github.com/wraith13/keep.grass"))),
-									OptionImageSource: Root.GetExportImageSource()
-								),
-							},
+							Version,
+							Auther,
+							Repository,
 						}
 					},
 				},
