cfe507909bab56bdff7b2332256362a0d277b93e
道化師 <wraith@trickpalace.net>
2016-11-03 05:04:36 +0900

    version 1.00.002

--- source/AlphaInfoPage.cs
+++ source/AlphaInfoPage.cs
@@ -23,7 +23,7 @@ namespace keep.grass
 				AlphaFactory.MakeCircleImageCell
 				(
 					ImageSource: Root.GetApplicationImageSource(),
-					Text: "1.00.001",
+					Text: "1.00.002",
 					Command: new Command
 					(
 						o => Device.OpenUri
