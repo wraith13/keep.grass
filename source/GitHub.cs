15811b679dd9d33457fdf9668d8f1a2de51a0ba5
道化師 <wraith@trickpalace.net>
2016-12-24 20:15:06 +0900

    GitHub フィードの型定義

--- source/GitHub.cs
+++ source/GitHub.cs
@@ -4,11 +4,36 @@ using System.Xml.Linq;
 using System.Linq;
 using System.Threading.Tasks;
 using System.IO;
+using System.Collections.Generic;
 
 namespace keep.grass
 {
 	public class GitHub
 	{
+		public class Feed
+		{
+			public class Link
+			{
+				public string Type { get; set; }
+				public string Rel { get; set; }
+				public string Href { get; set; }
+			}
+			public class Entry
+			{
+				public string Id { get; set; }
+				public DateTime Published { get; set; }
+				public DateTime Updated { get; set; }
+				public IEnumerable<Link> LinkList { get; set; }
+				public string Title { get; set; }
+			}
+
+			public string Id { get; set; }
+			public IEnumerable<Link> LinkList { get; set; }
+			public string Title { get; set; }
+			public DateTime Updated { get; set; }
+			public IEnumerable<Entry> EntryList { get; set; }
+		}
+
 		static private string BaseUrl = "https://github.com";
 
 		static private string ProfileUrlFormat = BaseUrl +"/{0}";
