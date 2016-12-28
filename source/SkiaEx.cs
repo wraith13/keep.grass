a32f732dad84569fad01695e73e82df04b96525a
道化師 <wraith@trickpalace.net>
2016-12-28 20:29:08 +0900

    SkiaはSVGに対応してるけどSkiaSharpは対応してなくて悲しみ

--- source/SkiaEx.cs
+++ source/SkiaEx.cs
@@ -25,26 +25,5 @@ namespace keep.grass
 				(byte)(c.A * 255)
 			);
 		}
-		static public ImageSource ImageFromSvg(int width, int height, byte[] source)
-		{
-			using (var Surface = SKSurface.Create(width, height, SKColorType.Rgba8888, SKAlphaType.Opaque))
-			{
-				using (var Data = new SKData(source))
-				{
-					using (var Bitmap = SKBitmap.Decode(Data))
-					{
-						Surface.Canvas.DrawBitmap(Bitmap, 0, 0);
-						using (var SnapShot = Surface.Snapshot())
-						{
-							using (var ResultData = SnapShot.Encode())
-							{
-								var Stream = new MemoryStream(ResultData.ToArray());
-								return ImageSource.FromStream(() => Stream);
-							}
-						}
-					}
-				}
-			}
-		}
 	}
 }
