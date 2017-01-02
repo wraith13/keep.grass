65ab78ebc6d055eb2b26a837b179960a028ec233
道化師 <wraith@trickpalace.net>
2017-01-03 08:34:48 +0900

    ユーザー検索( WIP )

--- source/GitHub.cs
+++ source/GitHub.cs
@@ -205,6 +205,33 @@ namespace keep.grass
 				return result;
 			}
 		}
+		public class SearchResult<T>
+		{
+			public long TotalCount { get; set; }
+			public bool IncompleteResults { get; set; }
+			public IEnumerable<T> Items { get; set; }
+		}
+		public class User
+		{
+			public string Login { get; set; }
+			public long Id { get; set; }
+			public string AvatarUrl { get; set; }
+			public string GravatarId { get; set; }
+			public string Url { get; set; }
+			public string HtmlUrl { get; set; }
+			public string FollowersUrl { get; set; }
+			public string FollowingUrl { get; set; }
+			public string GistsUrl { get; set; }
+			public string StarredUrl { get; set; }
+			public string SubscriptionsUrl { get; set; }
+			public string OrganizationsUrl { get; set; }
+			public string ReposUrl { get; set; }
+			public string EventsUrl { get; set; }
+			public string ReceivedEventsUrl { get; set; }
+			public string Type { get; set; }
+			public bool SiteAdmin { get; set; }
+			public double Score { get; set; }
+		}
 
 		static private string BaseUrl = "https://github.com";
 		static private string BaseApiUrl = "https://api.github.com";
