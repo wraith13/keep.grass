8f94451b05c4eb39ae778d9f9c555b25fd4abb5a
道化師 <wraith@trickpalace.net>
2017-01-04 20:59:46 +0900

    ユーザー検索( WIP )

--- source/GitHub.cs
+++ source/GitHub.cs
@@ -7,6 +7,7 @@ using System.Text.RegularExpressions;
 using System.IO;
 using System.Collections.Generic;
 using System.Diagnostics;
+using Newtonsoft.Json;
 
 namespace keep.grass
 {
@@ -207,48 +208,87 @@ namespace keep.grass
 		}
 		public class SearchResult<T>
 		{
+			[JsonProperty("total_count")]
 			public long TotalCount { get; set; }
+			[JsonProperty("incomplete_results")]
 			public bool IncompleteResults { get; set; }
+			[JsonProperty("items")]
 			public IEnumerable<T> Items { get; set; }
+
+			public static SearchResult<T> Parse(string Json)
+			{
+				return JsonConvert.DeserializeObject<SearchResult<T>>(Json);
+			}
 		}
 		public class BaseUser
 		{
+			[JsonProperty("login")]
 			public string Login { get; set; }
+			[JsonProperty("Id")]
 			public long Id { get; set; }
+			[JsonProperty("avatar_url")]
 			public string AvatarUrl { get; set; }
+			[JsonProperty("gravatar_id")]
 			public string GravatarId { get; set; }
+			[JsonProperty("url")]
 			public string Url { get; set; }
+			[JsonProperty("html_url")]
 			public string HtmlUrl { get; set; }
+			[JsonProperty("followers_url")]
 			public string FollowersUrl { get; set; }
+			[JsonProperty("following_url")]
 			public string FollowingUrl { get; set; }
+			[JsonProperty("gists_url")]
 			public string GistsUrl { get; set; }
+			[JsonProperty("starred_url")]
 			public string StarredUrl { get; set; }
+			[JsonProperty("subscriptions_url")]
 			public string SubscriptionsUrl { get; set; }
+			[JsonProperty("organizations_url")]
 			public string OrganizationsUrl { get; set; }
+			[JsonProperty("repos_url")]
 			public string ReposUrl { get; set; }
+			[JsonProperty("events_url")]
 			public string EventsUrl { get; set; }
+			[JsonProperty("received_events_url")]
 			public string ReceivedEventsUrl { get; set; }
+			[JsonProperty("type")]
 			public string Type { get; set; }
+			[JsonProperty("site_admin")]
 			public bool SiteAdmin { get; set; }
 		}
 		public class SearchUser : BaseUser
 		{
+			[JsonProperty("score")]
 			public double Score { get; set; }
 		}
 		public class User : BaseUser
 		{
+			[JsonProperty("name")]
 			public string Name { get; set; }
+			[JsonProperty("company")]
 			public string Company { get; set; }
+			[JsonProperty("blog")]
 			public string Blog { get; set; }
+			[JsonProperty("location")]
 			public string Location { get; set; }
+			[JsonProperty("email")]
 			public string Email { get; set; }
+			[JsonProperty("hireable")]
 			public bool Hireable { get; set; }
+			[JsonProperty("bio")]
 			public string Bio { get; set; }
+			[JsonProperty("public_repos")]
 			public int PublicRepos { get; set; }
+			[JsonProperty("public_gists")]
 			public int PublicGists { get; set; }
+			[JsonProperty("followers")]
 			public int Followers { get; set; }
+			[JsonProperty("following")]
 			public int Following { get; set; }
+			[JsonProperty("created_at")]
 			public DateTime CreatedAt { get; set; }
+			[JsonProperty("updated_at")]
 			public DateTime UpdatedAt { get; set; }
 		}
 
@@ -279,7 +319,7 @@ namespace keep.grass
 			return String.Format (IconUrlFormat, Id);
 		}
 
-		static private string SearchUsersUrlFormat = BaseApiUrl + "/search/users/?q={0}";
+		static private string SearchUsersUrlFormat = BaseApiUrl + "/search/users?q={0}";
 		static public string GetSearchUsersUrl(string Query)
 		{
 			return String.Format
