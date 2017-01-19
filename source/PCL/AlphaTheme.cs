78c103d3da0c8e0925ffe2d43ac3e13cc3d80b32
道化師 <wraith@trickpalace.net>
2017-01-20 00:14:48 +0900

    テーマ対応 ( WIP )

---
+++ source/PCL/AlphaTheme.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace keep.grass
+{
+    public class AlphaTheme
+    {
+        public Color ForeGroundColor
+        {
+            get;
+            private set;
+        }
+        public Color BackGroundColor
+        {
+            get;
+            private set;
+        }
+
+        private AlphaTheme()
+        {
+        }
+
+        public static AlphaTheme Light = new AlphaTheme
+        {
+            ForeGroundColor = Color.Black,
+            BackGroundColor = Color.White,
+        };
+        public static AlphaTheme Dark = new AlphaTheme
+        {
+            ForeGroundColor = Color.White,
+            BackGroundColor = Color.Black,
+        };
+
+        public static Dictionary<string, AlphaTheme> All = new Dictionary<string, AlphaTheme>
+        {
+            { nameof(Light), Light },
+            { nameof(Dark), Dark },
+        }
+    }
+}
