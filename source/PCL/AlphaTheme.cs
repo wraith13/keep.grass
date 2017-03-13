3d41bdeda42916aaa7dda1d018b5fe2e02b08518
道化師 <wraith@trickpalace.net>
2017-03-14 00:51:17 +0900

    テーマ対応 ( WIP )

--- source/PCL/AlphaTheme.cs
+++ source/PCL/AlphaTheme.cs
@@ -23,11 +23,6 @@ namespace keep.grass
             get;
             private set;
         }
-        public bool IsLightTheme
-        {
-            get;
-            private set;
-        }
 
         private AlphaTheme()
         {
@@ -38,14 +33,12 @@ namespace keep.grass
             AccentColor = Color.Default,// Color.FromRgb(0x44, 0x55, 0xEE),
             ForeGroundColor = Color.Black,
             BackGroundColor = Color.White,
-            IsLightTheme = true,
         };
         public static AlphaTheme Black = new AlphaTheme
         {
             AccentColor = Color.Default,// Color.FromRgb(0x44, 0x55, 0xEE),
             ForeGroundColor = Color.White,
             BackGroundColor = Color.Black,
-            IsLightTheme = false,
         };
 
         public static Dictionary<string, AlphaTheme> All = new Dictionary<string, AlphaTheme>
