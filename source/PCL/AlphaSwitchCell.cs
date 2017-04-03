1c069c4f821b6725cd1e819055944afdf0f5a3e4
道化師 <wraith@trickpalace.net>
2017-04-03 21:44:57 +0900

    💄 テーマ対応 ( WIP )

--- source/PCL/AlphaSwitchCell.cs
+++ source/PCL/AlphaSwitchCell.cs
@@ -13,33 +13,74 @@ namespace keep.grass
         bool On { get; set; }
         Cell AsCell();
     }
-    public class AlphaSwitchCell : SwitchCell, VoidSwitchCell
-    {
-        public new string Text
+
+    //  Xamarin.Forms 標準の SwitchCell だと TextColor が指定できないので自前実装
+    public class AlphaSwitchCell : ViewCell, VoidSwitchCell
+	{
+		protected Label TextLabel = new Label();
+        protected Switch Switch = new Switch();
+
+		public AlphaSwitchCell() : base()
+        {
+			View = new Grid().SetSingleChild
+			(
+				new StackLayout
+				{
+					Orientation = StackOrientation.Horizontal,
+					VerticalOptions = LayoutOptions.Center,
+					Padding = new Thickness(20, 2, 0, 2),
+					Children =
+					{
+						TextLabel,
+						Switch,
+					},
+				}
+			);
+
+			TextLabel.VerticalOptions = LayoutOptions.Center;
+			TextLabel.HorizontalOptions = LayoutOptions.StartAndExpand;
+			Switch.VerticalOptions = LayoutOptions.Center;
+			Switch.HorizontalOptions = LayoutOptions.End;
+			AlphaTheme.Apply(this);
+		}
+
+        public Color TextColor
         {
             get
             {
-                return base.Text;
+                return TextLabel.TextColor;
             }
             set
             {
-                base.Text = value;
+                TextLabel.TextColor = value;
             }
         }
-        public new bool On
-        {
-            get
-            {
-                return base.On;
-            }
-            set
-            {
-                base.On = value;
-            }
-        }
-        public Cell AsCell()
-        {
-            return this;
-        }
-    }
+
+		public new string Text
+		{
+			get
+			{
+                return TextLabel.Text;
+			}
+			set
+			{
+				TextLabel.Text = value;
+			}
+		}
+		public new bool On
+		{
+			get
+			{
+                return Switch.IsToggled;
+			}
+			set
+			{
+				Switch.IsToggled = value;
+			}
+		}
+		public Cell AsCell()
+		{
+			return this;
+		}
+	}
 }
