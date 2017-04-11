04c9aae34a7aa87c1b02a44fd9c6b84810ea80d3
道化師 <wraith@trickpalace.net>
2017-04-11 09:50:05 +0900

    🚧 Mac プロジェクトの作成

---
+++ source/keep.grass.Mac/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace keep.grass.Mac
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
