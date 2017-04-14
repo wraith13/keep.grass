80dd845a5c2039f7161e191852e8c9655558d405
chomado <chomado@gmail.com>
2017-04-15 02:41:12 +0900

    公開したくない鍵とかを記述するファイルを gitignore した

---
+++ source/PCL/Properties/KeySample.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace keep.grass.Properties
+{
+    public static class KeySample
+    {
+        // Mobile Center の iOSアプリの AppSecret文字列
+        public static readonly string MobileCenterSecretIos = "aaaa-aaaa-aaaa-aaa-aaaaaaa";
+
+        // Mobile Center の Android アプリの AppSecret文字列
+        public static readonly string MobileCenterSecretAndroid = "aaaaa-aaa-aaaa-aaaa-aaaaaaaaaaaaaa";
+    }
+}
