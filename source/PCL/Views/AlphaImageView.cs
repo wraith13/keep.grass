32411c14c8b257056da73522eb96232259dc2b45
道化師 <wraith@trickpalace.net>
2017-06-04 02:29:18 +0900

    🛠 アイコン表示を AlphaImageView ベースに ( WIP )

--- source/PCL/Views/AlphaImageView.cs
+++ source/PCL/Views/AlphaImageView.cs
@@ -1,53 +1,13 @@
 ﻿using System;
 using System.Linq;
 using System.Diagnostics;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 using SkiaSharp;
 using SkiaSharp.Views.Forms;
 using RuyiJinguBang;
-using System.Reflection;
 
 namespace keep.grass
 {
-    public abstract class ImageSource
-    {
-        public abstract Task<byte[]> Get();
-        public static ImageSource FromResource(Assembly Assembly, string Path)
-        {
-            return new ResourceImageSource(Assembly, Path);
-        }
-        public static ImageSource FromUri(string Url)
-        {
-            return new WebImageSource(Url);
-        }
-    }
-    public class ResourceImageSource : ImageSource
-    {
-        Assembly Assembly;
-        string Path;
-        public ResourceImageSource(Assembly a_Assembly, string a_Path)
-        {
-            Assembly = a_Assembly;
-            Path = a_Path;
-        }
-        public override async Task<byte[]> Get()
-        {
-            return await AlphaResouceProxy.Get(Assembly, Path);
-        }
-    }
-    public class WebImageSource : ImageSource
-    {
-        string Url;
-        public WebImageSource(string a_Url)
-        {
-            Url = a_Url;
-        }
-        public override async Task<byte[]> Get()
-        {
-            return await AlphaImageProxy.Get(Url);
-        }
-    }
     public abstract class VoidImage : SKCanvasView // プロパティのフィールドを明示的に指定するの避ける為だけのクラス
     {
         public virtual ImageSource ImageSource { get; set; }
