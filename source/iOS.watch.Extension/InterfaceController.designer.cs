a271543b9c2e1aa208290a956905a8e94c1d216d
道化師 <wraith@trickpalace.net>
2017-06-21 13:51:54 +0900

    🛠 iOS.watch 版 ( WIP )

--- source/iOS.watch.Extension/InterfaceController.designer.cs
+++ source/iOS.watch.Extension/InterfaceController.designer.cs
@@ -1,18 +1,28 @@
-﻿//
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// WARNING
 //
-
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
 
 namespace keep.grass.iOS.keep.grassExtension
 {
-    [Register("InterfaceController")]
+    [Register ("InterfaceController")]
     partial class InterfaceController
     {
-        void ReleaseDesignerOutlets()
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        WatchKit.WKInterfaceImage WatchCanvus { get; set; }
+
+        void ReleaseDesignerOutlets ()
         {
+            if (WatchCanvus != null) {
+                WatchCanvus.Dispose ();
+                WatchCanvus = null;
+            }
         }
     }
 }
